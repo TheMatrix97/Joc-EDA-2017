2596c558dc2405d0893a06314b75cac43124d9df
Marc catrisse <marc@catrisse.net>
2017-04-29 03:29:31 -0700

    hay que hacerlo mas listo

---
+++ AIMatrix2.cc
@@ -0,0 +1,160 @@
+#include "Player.hh"
+
+#include <cmath>
+#include <assert.h>
+#include <set>
+
+using namespace std;
+
+/**
+ * Write the name of your player and save this file
+ * with a name matching the format "AIyourname.cc".
+ */
+#define PLAYER_NAME Matrix2
+
+
+struct PLAYER_NAME : public Player {
+
+    /**
+     * Factory: returns a new instance of this class.
+     * Do not modify this function.
+     */
+    static Player* factory () {
+        return new PLAYER_NAME;
+    }
+
+
+    /**
+     * Attributes for your player can be defined here.
+     */     
+    //vector<int> my_awesome_vector_of_integers;
+
+
+    /**
+     * Play method.
+     * 
+     * This method will be invoked once per each round.
+     * You have to read the board and place your actions
+     * for this round.
+     */
+  /*   bool is_mybike(vector<int> motos_vecinas, int &i){
+		int i = 0;
+		for(int j = 0; j < (int)motos_vecinas.size(); j++){
+			if(bikes(motos_vecinas[j]).player == me()) {i = j; return true;}
+		}
+		return false;
+	 
+	 
+	 }*/
+
+     bool validar_vertex(int id, int my_bike_id){
+		 vector<int> vecinos = vertex(id).neighbours;
+		 bool pared = true; //damos por hecho que no sepuede ir pork es un camino sin salida
+		 for(int i = 0; i < (int)vecinos.size(); i++){
+			 if(vertex(vecinos[i]).bike != -1 and vertex(vecinos[i]).bike != my_bike_id) return false;
+			 if(vertex(vecinos[i]).wall == -1 and id != vecinos[i]) pared  = false; //no hay pared
+		 }
+		 if(pared) return false;
+		 return true;
+	 }
+		 
+
+	set<int> validar_vecinos(vector<int> &vecinos, int my_bike_id){
+		set<int> aux;
+		for(int i = 0; i < (int)vecinos.size(); i++){ //investigar si en ese vecino vacio cabe la poss de que llege una moto enemiga
+			if(validar_vertex(vecinos[i], my_bike_id)){
+				aux.insert(vecinos[i]);
+			}
+		}
+		return aux;
+	}
+					
+			
+			/*IDEAS:
+			 * MIRAR SCREENSHOT EN IMAGES, HAY QUE CAMBIAR EL CODIGO PARA EVITAR CRASHES, HABRIA QUE MIRAR SI EL VECINO X TIENE ALGUNA MOTO DE VECINO EXCEPTO TU, SI ES ASI NO QUEREMOS IR!!!
+			 * EN EL SCREEN LAS MOTOS NO SE ESQUIVAN PORK NO SON VECINAS*/
+			
+		
+		
+    void play () {
+		
+        vector<int> my_bikes = bikes(me());
+        for (int i = 0; i < (int)my_bikes.size(); ++i) {
+			
+			const Bike& my_bike = bike(my_bikes[i]);
+			
+			// Do something only if this bike is alive
+			if (!my_bike.alive) {
+				continue;
+			}
+			
+			//Only turbo bikes can move at odd rounds
+			if (round() % 2 && my_bike.turbo_duration <= 0) {
+				continue;
+			}
+			
+			// Find all empty neighbours
+			vector<int> neighbours = vertex(my_bike.vertex).neighbours;
+			vector<int> empty_neighbours;
+			vector<int> motos_vecinas;
+			
+			for (int i = 0; i < (int)neighbours.size(); i++) {
+				int id = neighbours[i];
+				if (vertex(id).wall == -1) {
+					empty_neighbours.push_back(id);
+				}
+				if(vertex(id).bike != -1) motos_vecinas.push_back(id);
+			}
+				
+			
+			// Create an empty movement
+			Movement movement(my_bike.id);
+			// Set next_vertex to a random empty neighbour, if any, or to a random neighbour otherwise
+			if (!empty_neighbours.empty()) { //mirar si tenemos una moto vecina, despues mirar si hay vecinos compartidos
+				set<int> vecinos_seguros = validar_vecinos(empty_neighbours,my_bike.id);
+				if(not vecinos_seguros.empty()){ //vamos a un pos random valida
+					cerr << "PELIGRO COLISION" << endl;
+					set<int>::iterator it;
+					//evitar efecto random, habria que hacer un 'radar' bfs? para ir a la zona con menos paredes
+					int ir_a = rand() % (int)vecinos_seguros.size();
+					int contador = 0;
+					for(it = vecinos_seguros.begin(); it != vecinos_seguros.end(); it++){
+						if(contador == ir_a)  movement.next_vertex = *it;
+						else contador++;
+					}
+				}
+				else{
+					movement.next_vertex = empty_neighbours[ rand() % (int)empty_neighbours.size() ];
+					
+				} 
+				
+			} else {
+				cerr << "no vecino libre" << endl;
+				//estaria guay poder prever possibles choques entre otros pilotos y evitar que se quede encerrado
+				movement.next_vertex = neighbours[ rand() % (int)neighbours.size() ];
+			}
+			
+			// Use bonus randomly when we have one
+			//bonus si ghost->habria que guardarlo para cuando no queden vecinos libres
+			//si el bonus es turbo usarlo en el momento
+			//si el bonus es puntos dobles esperar a que hayan pocos pilotos para evitar crash? :S
+			if (my_bike.bonus != None && rand()%5 > 3) {
+				movement.use_bonus = true;
+			}
+			
+			// Command the movement
+            command(movement);
+			
+        }
+        
+    }
+
+};
+
+
+
+/**
+ * Do not modify the following line.
+ */
+RegisterPlayer(PLAYER_NAME);
+
