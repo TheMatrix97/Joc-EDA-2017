253443c9cb7e738742531f5cfc04e6c9c9e43f4f
Marc catrisse <marc@catrisse.net>
2017-04-29 14:51:25 -0700

    bfs busqueda_obj funciona

--- AIMatrix2.cc
+++ AIMatrix2.cc
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <assert.h>
 #include <set>
+#include <list>
 
 using namespace std;
 
@@ -37,22 +38,58 @@ struct PLAYER_NAME : public Player {
      * You have to read the board and place your actions
      * for this round.
      */
-  /*   bool is_mybike(vector<int> motos_vecinas, int &i){
-		int i = 0;
-		for(int j = 0; j < (int)motos_vecinas.size(); j++){
-			if(bikes(motos_vecinas[j]).player == me()) {i = j; return true;}
+     /*
+	void busqueda_cami(int ini,vector<bool>&vis,list<int> &recc, int bonus){ //dfs busqueda bonuses
+		if(not vis[ini]){
+			vis[ini] = true;
+			recc.push_back(ini);
+			if (ini == bonus) return;
+			vector<int> vecinos;
+			vecinos = vertex(ini).neighbours;
+			for(int i = 0; i < (int)vecinos.size();i++){
+				if(vertex(vecinos[i]).wall == -1)
+					busqueda_cami(vecinos[i],vis,recc,bonus);
+				}
+			}
 		}
-		return false;
-	 
-	 
-	 }*/
+	
+	*/
+	list<int> busqueda_cami(int ini, int bonus){
+		vector<bool> vis(nb_vertices(),false);
+		queue< list<int> > cola;
+		list<int> aux;
+		aux.push_back(ini);
+		cola.push(aux);
+		while(not cola.empty()){
+			list<int> path = cola.front();
+			int last_nodo = path.back();
+			cola.pop();
+			vector<int> vecino_last = vertex(last_nodo).neighbours;
+			for(int i = 0; i < (int)vecino_last.size();i++){
+				list<int> new_path = path;
+				if(not vis[vecino_last[i]] and vecino_last[i] != last_nodo and vertex(vecino_last[i]).wall == -1){
+					vis[vecino_last[i]] = true;
+					new_path.push_back(vecino_last[i]);
+					if(vecino_last[i] == bonus) return new_path;
+					cola.push(new_path);
+				}
+				else if(vertex(vecino_last[i]).wall == -1) vis[vecino_last[i]] = true;
+			}
+		}
+		list<int> fail;
+		fail.push_back(-1);
+		return fail;
+					
+			
+	}
+		
 
      bool validar_vertex(int id, int my_bike_id){
 		 vector<int> vecinos = vertex(id).neighbours;
 		 bool pared = true; //damos por hecho que no sepuede ir pork es un camino sin salida
 		 for(int i = 0; i < (int)vecinos.size(); i++){
 			 if(vertex(vecinos[i]).bike != -1 and vertex(vecinos[i]).bike != my_bike_id) return false;
-			 if(vertex(vecinos[i]).wall == -1 and id != vecinos[i]) pared  = false; //no hay pared
+			 if(vertex(vecinos[i]).wall == -1 and bike(my_bike_id).vertex != vecinos[i]) pared  = false; //no hay pared
 		 }
 		 if(pared) return false;
 		 return true;
@@ -101,27 +138,41 @@ struct PLAYER_NAME : public Player {
 			for (int i = 0; i < (int)neighbours.size(); i++) {
 				int id = neighbours[i];
 				if (vertex(id).wall == -1) {
-					empty_neighbours.push_back(id);
+					if(id != my_bike.vertex) empty_neighbours.push_back(id);
 				}
 				if(vertex(id).bike != -1) motos_vecinas.push_back(id);
 			}
 				
+			//test bfs
+			list<int> recc;
+			vector<int>bonus = bonus_vertices();
+			cerr << "el bonus esta aqui: " << bonus[1] << endl;
+			cerr << "hay: " << nb_vertices() << endl;
+			cerr << "estoy en : " << my_bike.vertex << endl;
+			recc = busqueda_cami(my_bike.vertex,bonus[1]);
+			 while(not recc.empty()){
+				cerr << recc.front() << ' ';
+				recc.pop_front();
+			}
+			cerr << "fin" << endl;
 			
+					
 			// Create an empty movement
 			Movement movement(my_bike.id);
 			// Set next_vertex to a random empty neighbour, if any, or to a random neighbour otherwise
 			if (!empty_neighbours.empty()) { //mirar si tenemos una moto vecina, despues mirar si hay vecinos compartidos
 				set<int> vecinos_seguros = validar_vecinos(empty_neighbours,my_bike.id);
 				if(not vecinos_seguros.empty()){ //vamos a un pos random valida
-					cerr << "PELIGRO COLISION" << endl;
 					set<int>::iterator it;
-					//evitar efecto random, habria que hacer un 'radar' bfs? para ir a la zona con menos paredes
+					//evitar efecto random, recalculador de la ruta para ir a un punto especifico
 					int ir_a = rand() % (int)vecinos_seguros.size();
 					int contador = 0;
 					for(it = vecinos_seguros.begin(); it != vecinos_seguros.end(); it++){
 						if(contador == ir_a)  movement.next_vertex = *it;
 						else contador++;
 					}
+
+					
 				}
 				else{
 					movement.next_vertex = empty_neighbours[ rand() % (int)empty_neighbours.size() ];
@@ -129,7 +180,8 @@ struct PLAYER_NAME : public Player {
 				} 
 				
 			} else {
-				cerr << "no vecino libre" << endl;
+				cerr << "no vecino libre" << endl; //muerte segura, igual se puede evitar con ghost
+				if(my_bike.bonus == Ghost) movement.use_bonus = true;
 				//estaria guay poder prever possibles choques entre otros pilotos y evitar que se quede encerrado
 				movement.next_vertex = neighbours[ rand() % (int)neighbours.size() ];
 			}
@@ -138,9 +190,17 @@ struct PLAYER_NAME : public Player {
 			//bonus si ghost->habria que guardarlo para cuando no queden vecinos libres
 			//si el bonus es turbo usarlo en el momento
 			//si el bonus es puntos dobles esperar a que hayan pocos pilotos para evitar crash? :S
-			if (my_bike.bonus != None && rand()%5 > 3) {
-				movement.use_bonus = true;
+		
+			if(my_bike.bonus == Points) movement.use_bonus = true;
+			else if(my_bike.bonus == Turbo){
+				double aux;
+				aux = 0.75*nb_rounds(); //estamos a punto de acabar
+				if(round() >= aux){
+					movement.use_bonus = true;
+				}
 			}
+
+				
 			
 			// Command the movement
             command(movement);
